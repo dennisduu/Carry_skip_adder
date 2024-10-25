543215c74eeeff33bf8e87b6d18173cf2b9dd854
dennisduu <157513761+dennisduu@users.noreply.github.com>
2024-10-25 17:03:45 -0400

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -25,20 +25,24 @@ module tb ();
     forever #5 clk = ~clk; // 100 MHz clock
   end
 
+  // Declare power supply signals
+  supply1 VPWR;
+  supply0 VGND;
+
   // Instantiate the design under test (DUT)
   tt_um_carryskip_adder8 user_project (
-`ifdef GL_TEST
-      .VPWR(VPWR),
-    .VGND(VGND),
-`endif
-      .ui_in  (ui_in),    // Dedicated inputs
-      .uo_out (uo_out),   // Dedicated outputs
-      .uio_in (uio_in),   // IOs: Input path
-      .uio_out(uio_out),  // IOs: Output path
-      .uio_oe (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
-      .ena    (ena),      // enable - goes high when design is selected
-      .clk    (clk),      // clock
-      .rst_n  (rst_n)     // active low reset
-  );
+  `ifdef GL_TEST
+        .VPWR(VPWR),
+        .VGND(VGND),
+  `endif
+        .ui_in  (ui_in),    // Dedicated inputs
+        .uo_out (uo_out),   // Dedicated outputs
+        .uio_in (uio_in),   // IOs: Input path
+        .uio_out(uio_out),  // IOs: Output path
+        .uio_oe (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
+        .ena    (ena),      // enable - goes high when design is selected
+        .clk    (clk),      // clock
+        .rst_n  (rst_n)     // active low reset
+    );
 
 endmodule
